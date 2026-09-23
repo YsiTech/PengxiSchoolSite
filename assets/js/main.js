5d7f0fd665595dfc3e747e5c2d5a8a0db9dbc52f
YsiTech <1685805118@QQ.COM>
2026-09-23 22:22:05 +0800

    更新 main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,6 +28,7 @@
         { id:'videohub',   text:'视觉蓬中', href:'videohub.html' },
         { id:'campusnet',  text:'校园网',   href:'campusnet.html' },
         { id:'xingtu',     text:'星图',     href:'xingtu.html' },
+        { id:'blog',       text:'校园随笔', href:'blog.html' },
         { id:'moral',      text:'德育之窗', href:'moral.html' },
         { id:'admissions', text:'招生招聘', href:'admissions.html' },
         { id:'history',    text:'校史馆',   href:'history.html' },
