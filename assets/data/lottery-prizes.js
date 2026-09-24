632d373705d40a6c50c233ac7fa9ce192919e6d5
YsiTech <1685805118@QQ.COM>
2026-09-24 12:49:27 +0800

    更新 lottery-prizes.js

--- assets/data/lottery-prizes.js
+++ assets/data/lottery-prizes.js
@@ -1,6 +1,6 @@
 /* ===================================================================
    星图抽奖 · 奖品配置
-   （由管理后台导出 · 2026/9/24 12:37:36）
+   （由管理后台导出 · 2026/9/24 12:49:17）
    =================================================================== */
 
 window.LOTTERY_CONFIG = {
@@ -8,13 +8,13 @@ window.LOTTERY_CONFIG = {
   cost: 50,
 
   prizes: [
-    { id: 'p20', name: '20 亚斯卢布', type: 'money', reward: 20, weight: 45 },
-    { id: 'none', name: '再接再厉', type: 'none', reward: 0, weight: 60 },
-    { id: 'p50', name: '50 亚斯卢布', type: 'money', reward: 50, weight: 50 },
-    { id: 'p100', name: '100 亚斯卢布', type: 'money', reward: 100, weight: 15 },
-    { id: 'p200', name: '200 亚斯卢布', type: 'money', reward: 200, weight: 8 },
+    { id: 'p20', name: '20 亚斯卢布', type: 'money', reward: 20, weight: 55 },
+    { id: 'none', name: '再接再厉', type: 'none', reward: 0, weight: 70 },
+    { id: 'p50', name: '50 亚斯卢布', type: 'money', reward: 50, weight: 60 },
+    { id: 'p100', name: '100 亚斯卢布', type: 'money', reward: 100, weight: 25 },
+    { id: 'p200', name: '200 亚斯卢布', type: 'money', reward: 200, weight: 12 },
     { id: 'p500', name: '500 亚斯卢布', type: 'money', reward: 500, weight: 5 },
-    { id: 'gift', name: '精美小礼品', type: 'gift', reward: 0, weight: 3 },
+    { id: 'gift', name: '精美小礼品', type: 'gift', reward: 0, weight: 2 },
     { id: 'p1790195569199_264', name: '明月·中秋好礼', type: 'gift', reward: 0, weight: 1 },
     { id: 'p1790195657746_981', name: '2000 亚斯卢布', type: 'money', reward: 2000, weight: 1 }
   ]
